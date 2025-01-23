c0f95cf45a05ecfdab0a498eed765a2f215564f2
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-23 21:35:16 +0530

    Update in Java Code

--- day1.java
+++ day1.java
@@ -1,6 +1,6 @@
 public class day1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        // System.out.print("First Day in Java");
+        System.out.print("First Day in Java");
     }
 }
