31f0ef446177c429a0520bcb18bcd234638d6453
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-28 20:30:15 +0530

    showing output method

---
+++ Day 02/day02.java
@@ -0,0 +1,13 @@
+public class day02 {
+    public static void main(String[] args) {
+        // This print function show output in one line
+        System.out.print("one");
+        System.out.print("two");
+        System.out.println("three"); // ln has worked after the ln has used
+
+        // This println fuction show output in next line
+        System.out.println("four");
+        System.out.println("five");
+        System.out.println("six");
+    }
+}
