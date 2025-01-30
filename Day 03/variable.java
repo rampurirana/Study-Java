f6682841a9674ab5f1919a5dd0ba26b7381d264e
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-30 21:06:10 +0530

    Variable Declaration

---
+++ Day 03/variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String[] args) {
+        // Below is the method to declare variable in java
+        int age = 20;
+        // int = Data Type
+        // age = Variable Name
+        // 20 = Value
+
+        System.out.println(age);
+    }
+}
