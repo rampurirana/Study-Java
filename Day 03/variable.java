403a0ff988907059bb2cc4b7132fffd67d610c51
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-30 21:42:17 +0530

    Varaibles in Java

--- Day 03/variable.java
+++ Day 03/variable.java
@@ -1,5 +1,6 @@
 public class variable {
     public static void main(String[] args) {
+
         // Below is the method to declare variable in java
         int age = 20;
         // int = Data Type
