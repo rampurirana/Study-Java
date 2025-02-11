6ea9b9523767e4af3f914888027e1e2e9ea18bd1
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-11 21:30:33 +0530

    Non Primitive data types declaration

--- Day 04/DataTypes.java
+++ Day 04/DataTypes.java
@@ -10,7 +10,7 @@ public class DataTypes {
         char Character = 'R'; // char
         boolean isNumber = true; // boolean
 
-        // Generate output of these data types
+        // Generate output of premetive data types
         System.out.println("Byte Number : " + ByteNumber);
         System.out.println("Short Number : " + ShortNumber);
         System.out.println("Int Number : " + IntNumber);
@@ -19,5 +19,19 @@ public class DataTypes {
         System.out.println("Double Number : " + DoubleNumber);
         System.out.println("Character : " + Character);
         System.out.println("isNumber : " + isNumber);
+
+        // Non - Primitive Data Types Declaration
+        String fullName = "Rampravesh Rana";
+        int arrayNumbers[] = { 1, 2, 3, 4 };
+        /*
+         * class myClass {
+         * String shortName = "Rampuri";
+         * int age = 20;
+         * }
+         */
+
+        // Generate output of Non - premetive data types
+        System.out.println("Full Name : " + fullName);
+        System.out.println("Array : " + arrayNumbers[0]);
     }
 }
