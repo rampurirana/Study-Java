aec003acdad4832e8bc93e8d10805e1fd01a20cc
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-11 21:21:31 +0530

    Primitive data types declaration

---
+++ Day 04/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static void main(String args[]) {
+        // Primitive Data Types Declaration
+        byte ByteNumber = 100; // byte
+        short ShortNumber = 1500; // short
+        int IntNumber = 100000; // int
+        long LongNumber = 10000000L; // long
+        float FloatNumber = 10.5f; // float
+        double DoubleNumber = 100.35; // double
+        char Character = 'R'; // char
+        boolean isNumber = true; // boolean
+
+        // Generate output of these data types
+        System.out.println("Byte Number : " + ByteNumber);
+        System.out.println("Short Number : " + ShortNumber);
+        System.out.println("Int Number : " + IntNumber);
+        System.out.println("Long Number : " + LongNumber);
+        System.out.println("Float Number : " + FloatNumber);
+        System.out.println("Double Number : " + DoubleNumber);
+        System.out.println("Character : " + Character);
+        System.out.println("isNumber : " + isNumber);
+    }
+}
