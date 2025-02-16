a6b201d2491b25496a37af9061f284586b6390a3
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-16 19:33:25 +0530

    Taking input as user name and welcoming user

---
+++ Day 05/Questions/Q2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enater your name : ");
+        String name = input.nextLine();
+        System.out.println("Welcome " + name + " to learning Java.");
+    }
+}
