e63409eea6c31b3eaf9e3925580adc7c94554c47
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-16 18:45:13 +0530

    Solve questions using escape sequence

---
+++ Day 05/Questions/Q1.java
@@ -0,0 +1,7 @@
+// Solve program questions using escape sequence
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+    }
+}
