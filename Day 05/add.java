ea60230636a8b81e0ab3df31450304bd85fc487f
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-16 19:42:24 +0530

    Taking number input and adding these numbers

---
+++ Day 05/add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner add = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int num1 = add.nextInt();
+
+        System.out.print("Enter the second number : ");
+        int num2 = add.nextInt();
+
+        int result = num1 + num2;
+
+        System.out.print("The sum of two numbers : " + result);
+    }
+}
