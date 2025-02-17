5732ce4ff9685638f13b71366faddf7557b295f7
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-17 18:09:30 +0530

    Assignment operator

---
+++ Day 06/Operators/Assignment.java
@@ -0,0 +1,11 @@
+// Assignment operator : Assign the right hand operand's vlaue to the left hand operand's
+
+public class Assignment {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
