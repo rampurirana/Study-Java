1f2087e12110df756557bd93f4c6da6b95690167
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-17 18:55:04 +0530

    Arithmetic operators

---
+++ Day 06/Operators/Arithmetic.java
@@ -0,0 +1,14 @@
+// Arithmetic Operator : Arithmetic opertors are used to perform mathematical calculations.
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Addition : " + (a + b));
+        System.out.println("Subtraction : " + (a - b));
+        System.out.println("Multiplication : " + (a * b));
+        System.out.println("Division : " + (a / b));
+        System.out.println("Modulus : " + (a % b));
+    }
+}
