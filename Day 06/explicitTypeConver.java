f797057406a46b44297a5125081e01b08f62a9bd
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-17 17:48:26 +0530

    Explicit type casting

--- Day 06/explicitTypeConver.java
+++ Day 06/explicitTypeConver.java
@@ -1,8 +1,10 @@
+// 2. Explict type casting : Explicit type casting is manually convert a larger data type into a smaller data type
+//This is also called narrowing conversion and it may lead to data loss
+
 public class explicitTypeConver {
     public static void main(String[] args) {
-        double doubNum = 10.52d;
-        int intNum = (int) doubNum;
-
+        double doubNum = 10.52d; // double value(larger)
+        int intNum = (int) doubNum; // change into integer(smaller)
         System.out.println(intNum);
     }
 }
