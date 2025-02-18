f86bcbd6786e89663860c1784864c6f6dfd6b382
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-18 19:32:05 +0530

    Solved questions number three

---
+++ Day 07/Q3.java
@@ -0,0 +1,22 @@
+// Create a program to calculate perimeter of rectangle
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the distance between A & B : ");
+        int AB = input.nextInt();
+        System.out.print("Enter the distance between B & C : ");
+        int BC = input.nextInt();
+        System.out.print("Enter the distance between C & D : ");
+        int CD = input.nextInt();
+        System.out.print("Enter the distance between A & D : ");
+        int AD = input.nextInt();
+
+        int result = AB + BC + CD + AD;
+
+        System.out.println("Perimeter of rectangle is : " + result + "cm");
+    }
+}
