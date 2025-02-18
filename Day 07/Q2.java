6100d6ec720017fbc370af39b94181ccb6a37d65
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-18 19:18:41 +0530

    Solves question number two

---
+++ Day 07/Q2.java
@@ -0,0 +1,18 @@
+//Create a program to calculate product of two floating point numbers
+
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the first floating number : ");
+        float firstNum = input.nextFloat();
+        System.out.print("Please enter the second floating number : ");
+        float secondNum = input.nextFloat();
+
+        float result = firstNum * secondNum;
+
+        System.out.println("Prodict of two floating number : " + result);
+    }
+}
