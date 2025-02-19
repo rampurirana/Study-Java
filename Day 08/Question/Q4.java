50e190af808b1a1ce079c4620149dbdcc20be53f
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-19 19:08:21 +0530

    Convert forenheit to celcius

---
+++ Day 08/Question/Q4.java
@@ -0,0 +1,16 @@
+// Create a program to convert forenheit to celcius
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the temperature of forenheit : ");
+        int forenheit = input.nextInt();
+
+        double result = (forenheit - 32) * 5 / 9;
+
+        System.out.println("The temperature in celcius : " + result);
+    }
+}
