e9e32a59300ed00ab2f148c6e979626df5871366
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-24 18:18:34 +0530

    if-else conditional statement

---
+++ Day 09/ifElse.java
@@ -0,0 +1,18 @@
+// if-else : if-else statement is used for decision making. It allow the progrma to execute different block of code based on a condition
+
+import java.util.Scanner;
+
+public class ifElse {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your current age : ");
+        int age = input.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible for vote."); // age is greater and equal to 18
+        } else {
+            System.out.println("You are not eligible for vote"); // age is less than 18
+        }
+
+    }
+}
