6666eede7a72b17d1ed0b5e8fd22f6e637b4942f
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-24 19:26:55 +0530

    Relational Operators

---
+++ Day 09/RelationalOpe.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RelationalOpe {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int firstNum = input.nextInt();
+        System.out.print("Enter the first number : ");
+        int secondNum = input.nextInt();
+
+        // Checks the relation between these two numbers
+        System.out.println(firstNum + " == " + secondNum + " : " + (firstNum == secondNum)); // equal to
+        System.out.println(firstNum + " != " + secondNum + " : " + (firstNum != secondNum)); // not equal to
+        System.out.println(firstNum + " > " + secondNum + " : " + (firstNum > secondNum)); // greater than
+        System.out.println(firstNum + " < " + secondNum + " : " + (firstNum < secondNum)); // less than
+        System.out.println(firstNum + " >= " + secondNum + " : " + (firstNum >= secondNum)); // greater than or equal to
+        System.out.println(firstNum + " <= " + secondNum + " : " + (firstNum <= secondNum)); // less than or equal to
+    }
+}
