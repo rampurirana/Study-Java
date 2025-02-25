8f9381325fbce33e49444ddc500ec79c3008f389
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-25 19:01:41 +0530

    Create a program to determine positive, negative and zero number

---
+++ Day 10/Questions/Q1.java
@@ -0,0 +1,21 @@
+// Create a program to check if the number is positive or negative or zero
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the number : ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println("This number is positive");
+        } else if (number < 0) {
+            System.out.println("This number is negative");
+        } else {
+            System.out.println("This number is zero");
+        }
+
+    }
+}
