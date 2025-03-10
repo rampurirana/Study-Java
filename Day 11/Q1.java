c41d6ddd8cd43608e3fd51e26b9433c7e364b25f
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-10 20:11:25 +0530

    Updated the code for better result

--- Day 11/Q1.java
+++ Day 11/Q1.java
@@ -9,10 +9,14 @@ public class Q1 {
         System.out.print("Plese enter the year : ");
         int year = input.nextInt();
 
-        if (year % 4 == 0) {
+        if (year % 4 == 0 && year % 100 != 0) { // checking year like : 2024, 2025
+            System.out.println(year + " is a leap year.");
+        } else if (year % 400 == 0) { // checking year like : 1900, 2000, 2100
             System.out.println(year + " is a leap year.");
         } else {
-            System.out.println(year + " is not leap year.");
+            System.out.println(year + " is not a leap year.");
         }
+
+        input.close();
     }
 }
