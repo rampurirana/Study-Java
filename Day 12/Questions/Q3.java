eea110b50ac4e6e0523415aa2bb70fd2c509a890
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 19:03:17 +0530

    Create a program that shows bitwise XOR of two numbers

---
+++ Day 12/Questions/Q3.java
@@ -0,0 +1,12 @@
+// Create a program that shows bitwise XOR of two numbers
+
+public class Q3 {
+    public static void main(String[] args) {
+        int firstNumber = 6;
+        int secondNumber = 8;
+
+        int result = firstNumber ^ secondNumber;
+
+        System.out.println("Result of bitwise XOR : " + result);
+    }
+}
