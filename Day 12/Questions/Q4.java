8baf549af59312b7b788b36992df4f069db48bab
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 19:15:07 +0530

    / Create a program that shows bitwise NOT of two numbers

---
+++ Day 12/Questions/Q4.java
@@ -0,0 +1,14 @@
+// Create a program that shows bitwise NOT of two numbers
+
+public class Q4 {
+    public static void main(String[] args) {
+        int firstNumber = 20;
+        int secondNumber = 25;
+
+        int fristResult = ~firstNumber;
+        int secondResult = ~secondNumber;
+
+        System.out.println("Result of the bitwise NOT : " + fristResult);
+        System.out.println("Result of the bitwise NOT : " + secondResult);
+    }
+}
