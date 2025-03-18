0dcbe215e7ea8e5584ae37d00312dae7462e280d
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 19:18:05 +0530

    Create a program that shows use of left operator

---
+++ Day 12/Questions/Q5.java
@@ -0,0 +1,11 @@
+// Create a program that shows use of left operator
+
+public class Q5 {
+    public static void main(String[] args) {
+        int number = 10;
+
+        int result = number << 2;
+
+        System.out.println("Result of left shift operator : " + result);
+    }
+}
