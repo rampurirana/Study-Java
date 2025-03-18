e43c6c5efa1b142981ebd18f08008d4b091fbd70
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 18:59:30 +0530

    Create a program that shows bitwise OR of two numbers

---
+++ Day 12/Questions/Q2.java
@@ -0,0 +1,12 @@
+// Create a program that shows bitwise OR of two numbers
+
+public class Q2 {
+    public static void main(String[] args) {
+        int firstNumber = 8;
+        int secondNumber = 4;
+
+        int result = firstNumber | secondNumber;
+
+        System.out.println("Result of bitwise OR : " + result);
+    }
+}
