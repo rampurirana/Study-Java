7797efb5dfe56530d6f765bc4c630dd77ef63a29
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 19:22:25 +0530

    Create a program that shows use of right shift operator

---
+++ Day 12/Questions/Q6.java
@@ -0,0 +1,11 @@
+// Create a program that shows use of right shift operator
+
+public class Q6 {
+    public static void main(String[] args) {
+        int number = 15;
+
+        int result = number >> 2;
+
+        System.out.println("Result of right shift operator : " + result);
+    }
+}
