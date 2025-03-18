4a142e561c566e0034af9c38c20f444bd2ad941f
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 18:57:04 +0530

    Create a program that shows bitwise AND of two numbers

---
+++ Day 12/Questions/Q1.java
@@ -0,0 +1,12 @@
+// Create a program that shows bitwise AND of two numbers
+
+public class Q1 {
+    public static void main(String[] args) {
+        int firstNumber = 8;
+        int secondNumber = 12;
+
+        int result = firstNumber & secondNumber;
+
+        System.out.println("The bitwise AND of " + firstNumber + " and " + secondNumber + " : " + result);
+    }
+}
