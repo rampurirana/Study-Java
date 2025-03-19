40afebb34ca35b0ba14f8dc073e03c6905b26dbe
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-19 16:56:39 +0530

    while loop in java

---
+++ Day 14/whileLoop.java
@@ -0,0 +1,11 @@
+// 2. while loop : Used when the number of iteration is unknown and depends on the condition
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int i = 0; // initialized
+        while (i < 5) { // condition
+            System.out.println(i);
+            i++;
+        }
+    }
+}
