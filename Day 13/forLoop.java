9104a15e223abe884f36cbf183516cd99147cda0
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-19 16:53:30 +0530

    for loop in java

---
+++ Day 13/forLoop.java
@@ -0,0 +1,10 @@
+// Loop : Loop is a control flow statement that allow code to be executed repeatedly based on a conditon
+
+// 1. for loop : Used when the number of iteration is known
+public class forLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) { // i=0: initialized, i<5: conditon, i++: increasing
+            System.out.println(i);
+        }
+    }
+}
