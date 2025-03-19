0312cd8e57a1f18dd9b799389fabc2008c6acc1d
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-19 17:00:57 +0530

    do while loop in java

---
+++ Day 13/doWhileLoop.java
@@ -0,0 +1,11 @@
+// 3. do while loop : Similar to while, but ensure the code runs at once time
+
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int i = 0; // initialized
+        do {
+            System.out.println(i);
+            i++; // unary operator
+        } while (i < 5); // condition
+    }
+}
